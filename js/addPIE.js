8a8f381cdcf82e0e2f99726d7997773e622a5f0d
Froilan Quispe Gutierrez <froilan.q@gmail.com>
2011-11-25 13:18:05 +0100

    primera version

---
+++ js/addPIE.js
@@ -0,0 +1,29 @@
+function getStyleProperty(element, styleProperty)
+{
+  var prop="";
+  if(element.currentStyle)
+  {
+    prop=element.currentStyle[styleProperty];
+  }
+  else if(window.getComputedStyle)
+  {
+    prop=document.defaultView.getComputedStyle(element,null).getPropertyValue(styleProperty);
+  }
+  return prop;
+}
+
+// When the page is loaded
+$(function()
+{
+  // attach PIE on elements with CSS3
+  if(window.PIE)
+  {
+    $("body *").each(function()
+    {
+     if(getStyleProperty(this,'border-radius')||getStyleProperty(this,'box-shadow')||getStyleProperty(this,'-pie-background'))
+     {
+       PIE.attach(this);
+     }
+    });
+  }
+});
